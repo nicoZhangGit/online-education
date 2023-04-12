af67389a53e6fae6687d1c5c06e97b99811cff27
nico Zhang <izhangjunforwork@outlook.com>
2023-04-12 10:50:55 +0800

    数据模型

---
+++ online-education-content/online-education-content-model/src/main/java/com/zj/OnlineEducationContentModelApplication.java
@@ -0,0 +1,13 @@
+package com.zj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEducationContentModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineEducationContentModelApplication.class, args);
+    }
+
+}
